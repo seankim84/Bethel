959fd769d3061dcb1220ac342835384549563af5
SeanKim <sean.kim84@gmail.com>
2018-09-28 10:34:27 +0700

    Fetching the data

--- pages/ssr-test.js
+++ pages/ssr-test.js
@@ -1,17 +1,26 @@
 import Layout from '../components/Layout';
+import axios from 'axios';
+
 
 class SSRTest extends React.Component {
     static async getInitialProps ({req}) {
-        return req
-        ? { from: 'server' }
-        : { from: 'client' }
-    }   
-    render(){
-        return(
-            <Layout>
-                {this.props.from} 에서 실행이 되었어요
-            </Layout>
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        return {
+            users: response.data
+        }
+    }
+    render() {
+        const { users } = this.props;
+        const userList = users.map(
+            user => <li key={user.id}>{user.username}</li>
         )
+        return (
+            <Layout>
+                <ul>
+                    {userList}
+                </ul>
+            </Layout>
+        );
     }
 }
 
