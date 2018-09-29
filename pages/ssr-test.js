faeeed9f90e84ea8e5ee6a622db67b6f67de4020
SeanKim <sean.kim84@gmail.com>
2018-09-29 14:45:32 +0700

    Fixing the Axios

--- pages/ssr-test.js
+++ pages/ssr-test.js
@@ -2,18 +2,22 @@ import Layout from '../components/Layout';
 import axios from 'axios';
 
 
+
 class SSRTest extends React.Component {
     static async getInitialProps ({req}) {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const response = await axios.get('http://api.football-data.org/v2/competitions');
         return {
             users: response.data
         }
     }
+
     render() {
         const { users } = this.props;
+
         const userList = users.map(
             user => <li key={user.id}>{user.username}</li>
         )
+        
         return (
             <Layout>
                 <ul>
